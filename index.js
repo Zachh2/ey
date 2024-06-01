e67305267009599a1ae53c87c671b31b7fdd791e
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-06-01 17:56:33 +0800

    sda

--- index.js
+++ index.js
@@ -394,7 +394,7 @@ async function accountLogin(state, enableCommands = [], prefix, admin = []) {
 							const { threadID } = event;
 
 					if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == userid)) {
-					api.changeNickname(`[ ${prefix} ] CHATBOTV5`, threadID, userid);
+					api.changeNickname(`[ ${prefix} ] ChatBotV5`, threadID, userid);
 
 let gifUrls = [
 	'https://i.imgur.com/209z0iM.mp4',
--- script/ai2.js
+++ script/ai2.js
@@ -13,32 +13,31 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args }) {
-
   const input = args.join(' ');
 
-
-  
   if (!input) {
-    api.shareContact(`𝑯𝑬𝑳𝑳𝑶 𝑰𝑴 GPT4 (Api shiki machina)
-
-━━━━━━━━━━━━━━━
-
- 𝑷𝑳𝑬𝑨𝑺𝑬 𝑷𝑹𝑶𝑽𝑰𝑫𝑬 𝑨 𝑸𝑼𝑬𝑺𝑻𝑰𝑶𝑵/𝑸𝑼𝑬𝑹𝒀`,api.getCurrentUserID(), event.threadID, event.messageID);
+    api.sendMessage({
+      body: "𝑯𝑬𝑳𝑳𝑶 𝑰𝑴 GPT4 (Api shiki machina)\n\n━━━━━━━━━━━━━━━\n\n𝑷𝑳𝑬𝑨𝑺𝑬 𝑷𝑹𝑶𝑽𝑰𝑫𝑬 𝑨 𝑸𝑼𝑬𝑺𝑻𝑰𝑶𝑵/𝑸𝑼𝑬𝑹𝒀",
+    }, event.threadID, event.messageID);
     return;
   }
-  api.setMessageReaction("⏳", event.messageID, (err) => {
-  }, true);
-api.sendTypingIndicator(event.threadID, true);
 
-  api.shareContact(`🔍𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩....
-━━━━━━━━━━━━━━━━━━\n\n "${input}"`,api.getCurrentUserID(),event.threadID, event.messageID);
-  
+  api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+  api.sendTypingIndicator(event.threadID, true);
+
+  api.sendMessage({
+    body: `🔍𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩....\n━━━━━━━━━━━━━━━━━━\n\n"${input}"`,
+  }, event.threadID, event.messageID);
+
   try {
     const { data } = await axios.get(`https://gpt4o.onrender.com/gpt4o?question=${encodeURIComponent(input)}`);
-    let response = data.response;
-    response += "\n\n";
-    api.shareContact(response,api.getCurrentUserID(), event.threadID, event.messageID);
+    const response = JSON.stringify(data, null, 2); // Convert JSON response to string with indentation for readability
+    api.sendMessage({
+      body: response,
+    }, event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+  } finally {
+    api.sendTypingIndicator(event.threadID, false); // Stop typing indicator
   }
 };
